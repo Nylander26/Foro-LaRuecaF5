73cf41abbb5d57cbdacb6a3fb56ecd6672db76c7
Carlos Trias F <carlostrias112@gmail.com>
2022-07-27 14:27:35 +0200

    index html

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <!--Inicio de Sesion PHP-->
 
-<?php
-session_start();
-?>
+
 
 <!--Formato HTML-->
 <!DOCTYPE html>
@@ -11,66 +9,17 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
+    <link rel="stylesheet" href="./styles/index.css">
+    <title>forYOU</title>
 </head>
 <body>
-    
-    <!--Definir Usuario en caso de loguearse de manera exitosa y de ser exitoso el login se muestra el cuadro de publicacion-->
-    <?php
-    if(isset($_SESSION["usuario"])){
-    ?>
-
-    <?php include("config/top.php"); ?>
-
-    <br><br>
-
-    <!--Cuadro de Texto para compartir publicaciones-->
-    <form action="" method="POST">
-        <textarea name="mensaje" id="" cols="50" rows="8" placeholder="Que estas pensando?"></textarea>
-        <br>
-        <input type="submit" name="share" value="Compartir">
-    </form>
-
-    <!--Almacenar publicaciones mediante la funcionalidad del boton SHARE-->
-    <?php
-        
-        if(isset($_POST["share"])){
-            
-            require("config/config.php");
-
-            $share = $_POST["mensaje"];
-
-            $insertar = $conexion->query("INSERT INTO publicaciones (mensaje, usuario, fecha) VALUES ('$share', '".$_SESSION['id']."', now())");
-        }
-    ?>
+    <div class="divIndex">
+    <img src="img/logo1.png"class="logo">
     <br>
-
-    <!--Mostrar publicaciones almacenadas anteriormente y mostrando el nombre de usuario-->
-    <?php
-
-        require("config/config.php");
-
-        $mensajes = $conexion->query("SELECT * FROM publicaciones ORDER BY id desc");
-        while ($row = $mensajes->fetch_assoc()){ 
-            
-            $usuarioName = $conexion->query("SELECT usuario FROM usuarios WHERE id = '".$row['usuario']."'");
-            $rowUser = $usuarioName->fetch_assoc();
-            ?>
-
-            <div>
-                <div><?php echo $row["mensaje"]; ?></div>
-                <div><a href="perfil.php?id=<?php echo $row['usuario']; ?>"><?php echo $rowUser["usuario"]; ?></a></div>
-            </div>
-            <br>
-
-        <?php } ?>
-
-        <!--Si el usuario no esta logueado no se muestra el cuadro de texto para publicaciones ni se muestran las publicaciones realizadas-->
-        <?php
-
-         } else{
-        echo "<a href='login.php'>Debes loguearte</a> o <a href='register.php'>Debes registrarte</a>";
-    }
-    ?>
+    <div class="divButtonIndex">
+       <a href='login.php'><button class='indexButton'> Inicio de Sesión</button></a> 
+       <a href='register.php'><button class="indexButton">Registro</button></a>
+    </div>
+    </div>
 </body>
 </html>
