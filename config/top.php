f2bd5889225a0c24ce58d4c007cb60e0b8855637
Wolfgang <wolfnylander@gmail.com>
2022-08-20 19:56:07 +0200

    borrado top.css y mods top.php

--- config/top.php
+++ config/top.php
@@ -6,16 +6,16 @@
 
 ?>
 <?php if ($_GET["id"] == $_SESSION["id"]){ ?>
-    <div style = "width: 100%; float: left; margin-bottom: 30px;">
+    <div style = "width: 100%; margin-bottom: 3%; display: flex; justify-content: space-around;">
         <div style= "float: left; width: 20%;"><?php echo "Bienvenido ". $row['usuario']."<br><br>"; ?></div>
         <div style= "float: left; width: 20%;"><a href="index.php?id=<?php echo $_SESSION['id']; ?>">Home</a></div>
         <div style= "float: left; width: 20%;"><a href="perfil.php?id=<?php echo $_SESSION['id']; ?>">Mi Perfil</a></div>
         <div style= "float: left; width: 20%;"><a href='logout.php'>Salir</a></div>
     </div>
-<?php } else{ ?>
-    <div style = "width: 100%; float: left; margin-bottom: 30px;">
+<?php } /*else{ ?>
+    <div style = "width: 100%; margin-bottom: 3%; display: flex; justify-content: space-around;">
         <div style= "float: left; width: 20%;"><a href="index.php?id=<?php echo $_SESSION['id']; ?>">Home</a></div>
         <div style= "float: left; width: 20%;"><a href="perfil.php?id=<?php echo $_SESSION['id']; ?>">Mi Perfil</a></div>
         <div style= "float: left; width: 20%;"><a href='logout.php'>Salir</a></div>
     </div>
-<?php } ?>
+<?php } */?>
