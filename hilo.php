661b9931f58fb993ab1b7d91272c5f239e5eb86b
Wolfgang <wolfnylander@gmail.com>
2022-08-21 18:58:16 +0200

    varios mods

--- hilo.php
+++ hilo.php
@@ -8,7 +8,15 @@
 </head>
 <body>
 
+<?php
 
+    session_start();
+
+    if(!$_SESSION["usuario"]){
+        header("Location: login.php");
+    }
+    include("config/top.php");
+?>
 <!--Cuadro de Texto para compartir publicaciones -->
     <form action="" method="POST">
     <input type="text" name="descHilo" placeholder="Descripción"><br>
@@ -20,8 +28,8 @@
     <!--Almacenar publicaciones mediante la funcionalidad del boton SHARE-->
     
     <?php
-        $idUsuario=$_SESSION["id"];
-        print_r ($idUsuario);
+        $idUsuario = $_SESSION["id"];
+        //print_r ($idUsuario);
         
    if(isset($_POST["share"])){
             
@@ -29,11 +37,11 @@
             $descHilo=$_POST['descHilo'];
             $conHilo = $_POST["contHilo"];
 
-            $insertar = $conexion->query("INSERT INTO hilos (hilos, contHilo, idUsuario, idTemas) VALUES ('$descHilo', '$conHilo', '".$_SESSION['id']."', '$conHilo'");
+            $insertar = $conexion->query("INSERT INTO hilos (hilos, contHilo, idUsuario, idTemas) VALUES ('$descHilo', '$conHilo', '$idUsuario', '$idUsuario')");
         }
+        print_r($_SESSION["id"]);
     ?>
+    <a href="index.php?id=<?php echo $_SESSION['id']; ?>">home</a>
     <br>
-
-
 </body>
 </html>
--- temas.php
+++ temas.php
@@ -1,55 +1,56 @@
-<br>
-            <form action="hilo.php" method="POST">
-            <input type="hidden" name="newHilo" value="<?php echo $row["id"]; ?>">  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    
+    require("config/config.php");
+    $idTema= $_POST['idTema'];
+    $hilos = $conexion->query("SELECT * FROM hilos WHERE idTemas = '$idTema' "); //consulta los Hilos asociados al Tema que se está viendo desde POST ?>
+        
+        <form action="hilo.php?id=<?php echo $_SESSION['id'];?>" method="POST">
+            <input type="hidden" value="<?php $_SESSION["id"];?>">  
             <input type="submit" value='Nuevo Hilo'>
-            </form>                
-
-<?php
-
-require("config/config.php");
-$idTema= $_POST['idTema'];
-$hilos = $conexion->query("SELECT * FROM hilos WHERE idTemas = '$idTema' ");//consulta los Hilos asociados al Tema que se está viendo desde POST
-
-
-
-if ($_POST['idTema'] == $row["id"])
-{
-
-    while ($row = $hilos->fetch_assoc())
-    { 
-        $usuarioName = $conexion->query("SELECT usuario FROM usuarios WHERE id = '".$row['idUsuario']."'");
-        $rowUser = $usuarioName->fetch_assoc();
-            ?>
-
-        <!--Imprime tantos hilos existan en el Tema -->
-    <div class="divHilo" >
-
-        <h3> <?php echo $row["hilos"]; ?> </h3>
-        <h4> Publicado por: <?php echo $rowUser["usuario"]; ?> </h4>
-        <!--<img src="<?php echo $rowUser["imagen"];?>">-->
-        
-        <?php 
-        $idHilo= $row["id"];
-        $comentarios = $conexion->query("SELECT * FROM comentarios WHERE idHilo = '$idHilo'");
-        while ($rowC = $comentarios->fetch_assoc())
-        { 
-        ?>
-        <div class="divComentario">  <?php echo $rowC["comentario"];  ?>
-        </div>
-
-    </div>
+        </form>                
     
-        
-        <?php } //Cierre while comentario ?>
-        <br>
-
-    <?php }//Cierre while Hilos ?>
-
+        <?php if ($idTema == $row["id"]) {
     
-    </div>
-   
+            while ($row = $hilos->fetch_assoc()){
+    
+                $usuarioName = $conexion->query("SELECT usuario FROM usuarios WHERE id = '".$row['idUsuario']."'");
+                $rowUser = $usuarioName->fetch_assoc(); ?>
+    
+                <!--Imprime tantos hilos existan en el Tema -->
+    
+                <div class="divHilo" >
+                    <h3> <?php echo $row["hilos"]; ?> </h3>
+                    <h4> Publicado por: <?php echo $rowUser["usuario"]; ?> </h4>
+                    <!--<img src="<?php echo $rowUser["imagen"];?>">-->
+                    
+                    <?php 
+                        $idHilo= $_SESSION["id"];
+                        $comentarios = $conexion->query("SELECT * FROM comentarios WHERE idHilo = '$idHilo'");
 
-    <?php } //Cierre IF ?>
+                        while ($rowC = $comentarios->fetch_assoc()) { 
+                    ?>
+                    <div class="divComentario">  
+                        <?php echo $rowC["comentario"];?>
+                    </div>
+                </div>
+                    <?php } //Cierre while comentario ?>
+                <br>
+            <?php }//Cierre while Hilos ?>
+            <input type="submit" value="Agregar Comentario">
+            </div>
+        <?php } //Cierre IF ?>
+</body>
+</html>
    
            
 
